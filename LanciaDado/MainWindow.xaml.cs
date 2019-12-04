40e882d22a1752f80ca89cb11e6213ec087446cc
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-04 12:58:02 +0100

    Add project files.

---
+++ LanciaDado/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LanciaDado
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtNumero.Focus();
+        }
+
+        private void bunVerifica_Click(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(txtNumero.Text);
+                if (a > 6 || a < 1)
+                {
+                    MessageBox.Show("il numero inserito non è corretto", "Errore", MessageBoxButton.OK, MessageBoxImage.Error);
+                    txtNumero.Clear();
+                }
+                else
+                {
+                    Random r = new Random();
+                    int b = r.Next(1, 6);
+                    if (a == b)
+                    {
+                        txtRisultato.Text = ("Hai vinto");
+                    }
+                    else
+                        txtRisultato.Text = ("Hai perso");
+                    a = 0;
+                    b = 0;
+                }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LancioDado
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(RoutedEventArgs e, object sender)
+        {
+            double a = double.Parse(txtNumero.Text);
+            if (a > 6 || a < 1)
+            {
+                MessageBox.Show("il numero inserito non è corretto, "Errore", MessageBoxButton.OK, MessageBox.Error);
+                    txt.Numero.Clear()
+            }
+
+            
+            
+        }
+
+        
+        
+
+        
+    }
+}
